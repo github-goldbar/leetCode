6bdf70e1b369f40c61f4d7bcfc5dbfae099599db
ccheng <ccheng>
2021-08-23 09:44:35 +0800

    submit OddEvenLinkedList & RemoveLinkedListElements

---
+++ src/main/java/list/linkedlist/OddEvenLinkedList.java
@@ -0,0 +1,58 @@
+package list.linkedlist;
+
+public class OddEvenLinkedList implements Cloneable{
+    public static void main(String[] args) {
+
+        ListNode nodeA = new ListNode(1);
+        ListNode headA = nodeA;
+
+        nodeA.next = new ListNode(2);
+        nodeA = nodeA.next;
+
+        nodeA.next = new ListNode(3);
+        nodeA = nodeA.next;
+
+        nodeA.next = new ListNode(4);
+        nodeA = nodeA.next;
+
+        nodeA.next = new ListNode(5);
+//        nodeA = nodeA.next;
+//
+//        nodeA.next = new ListNode(6);
+
+
+        Utils.countNodeLength(oddEvenList(headA));
+//        Utils.countNodeLength(headA);
+
+    }
+
+    public static ListNode oddEvenList(ListNode head) {
+        int count = 1;
+
+        ListNode oddHead = new ListNode(-1);
+        ListNode odd = oddHead;
+
+        ListNode evenHead = new ListNode(-1);
+        ListNode even = evenHead;
+
+        while (head != null) {
+            if (count % 2 != 0) {
+                odd.next = head;
+                odd = odd.next;
+            } else {
+                even.next = head;
+                even = even.next;
+            }
+            count++;
+            head = head.next;
+        }
+
+        if (count % 2 == 0) {
+            even.next = null;
+        }
+
+        odd.next = evenHead.next;
+
+        return oddHead.next;
+    }
+}
---
+++ src/main/java/list/linkedlist/RemoveLinkedListElements.java
@@ -0,0 +1,114 @@
+package list.linkedlist;
+
+public class RemoveLinkedListElements {
+    public static void main(String[] args) {
+//        Example 1:
+//
+//        Input: head = [1,2,6,3,4,5,6], val = 6
+//        Output: [1,2,3,4,5]
+
+        ListNode nodeA = new ListNode(1);
+        ListNode headA = nodeA;
+
+        nodeA.next = new ListNode(2);
+        nodeA = nodeA.next;
+
+        nodeA.next = new ListNode(6);
+        nodeA = nodeA.next;
+
+        nodeA.next = new ListNode(3);
+        nodeA = nodeA.next;
+
+        nodeA.next = new ListNode(4);
+        nodeA = nodeA.next;
+
+        nodeA.next = new ListNode(5);
+        nodeA = nodeA.next;
+
+        nodeA.next = new ListNode(6);
+
+//        removeElements(headA, 6);
+
+        Utils.countNodeLength(headA);
+
+//        Example 2:
+//
+//        Input: head = [], val = 1
+//        Output: []
+
+//        ListNode nodeB = new ListNode(2);
+//        removeElements(nodeB, 1);
+
+//        Example 3:
+//
+//        Input: head = [7,7,7,7], val = 7
+//        Output: []
+
+        ListNode nodeB = new ListNode(7);
+        ListNode headB = nodeB;
+
+        nodeB.next = new ListNode(7);
+        nodeB = nodeB.next;
+
+        nodeB.next = new ListNode(7);
+        nodeB = nodeB.next;
+
+        nodeB.next = new ListNode(7);
+
+        removeElements(headB, 7);
+
+        Utils.countNodeLength(headB);
+
+//        Constraints:
+//
+//        The number of nodes in the list is in the range [0, 10^4].
+//        1 <= Node.val <= 50
+//        0 <= val <= 50
+    }
+
+    public static ListNode removeElements(ListNode head, int target) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+
+        ListNode current = head;
+        ListNode previous = fakeHead;
+
+        while (current != null) {
+            if (current.val == target){
+                previous.next = current.next;
+            } else {
+                previous = previous.next;
+            }
+            current = current.next;
+        }
+
+        return fakeHead.next;
+
+//        ListNode fakeHead = new ListNode(0);
+//        fakeHead.next = head;
+//
+//        ListNode slowRunner = fakeHead;
+//        ListNode fastRunner = fakeHead.next;
+//        while (fastRunner.next != null) {
+//            while (fastRunner.val != target) {
+//                slowRunner = slowRunner.next;
+//                fastRunner = fastRunner.next;
+//            }
+//
+//            while (fastRunner.val == target) {
+//                fastRunner = fastRunner.next;
+//            }
+//
+//            slowRunner.next = fastRunner;
+//        }
+//
+//        return head;
+    }
+
+
+
+}
